a23f5283e499550925a4416e13abf08497dc9cd8
Waqas Ali <waqas_111@hotmail.com>
2019-08-13 16:38:21 +0500

    add user details route

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,12 @@ const app = require("express")();
 const FBAuth = require("./util/fbAuth");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage } = require("./handlers/users");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails
+} = require("./handlers/users");
 
 // import firebase from "firebase";
 
@@ -18,6 +23,7 @@ app.post("/scream", FBAuth, postOneScream);
 app.post("/signup", signup);
 app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
 // https://baseurl.com/api/screams
 
 exports.api = functions.https.onRequest(app);
